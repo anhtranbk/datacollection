bdcb86f99dedac808f0fe3aff32c6241784666df
tjeubaoit <anhtn.bk@gmail.com>
2018-04-16 22:01:02 +0700

    [integration] Add Thrift helper

---
+++ integration/src/main/java/com/vcc/bigdata/platform/thrift/ThriftRuntimeException.java
@@ -0,0 +1,24 @@
+package com.vcc.bigdata.platform.thrift;
+
+/**
+ * TODO: Class description here.
+ *
+ * @author <a href="https://github.com/tjeubaoit">tjeubaoit</a>
+ */
+public class ThriftRuntimeException extends RuntimeException {
+
+    public ThriftRuntimeException() {
+    }
+
+    public ThriftRuntimeException(String message) {
+        super(message);
+    }
+
+    public ThriftRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ThriftRuntimeException(Throwable cause) {
+        super(cause);
+    }
+}

b611a4043cb9e9f05affb12533be6d0890d4c962
tjeubaoit <anhtn.bk@gmail.com>
2018-05-03 22:17:43 +0700

    [core] Add stream extractor

---
+++ core/src/main/java/com/vcc/bigdata/extract/DataStream.java
@@ -0,0 +1,18 @@
+package com.vcc.bigdata.extract;
+
+import java.io.Closeable;
+import java.util.Iterator;
+
+/**
+ * TODO: Class description here.
+ *
+ * @author <a href="https://github.com/tjeubaoit">tjeubaoit</a>
+ */
+public interface DataStream<T> extends Closeable, Iterator<T> {
+
+    boolean hasNext();
+
+    T next();
+
+    void close();
+}

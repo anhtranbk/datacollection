7da035df0550a0d0065a1d978ae40a87ad55862b
tjeubaoit <anhtn.bk@gmail.com>
2018-05-21 21:21:22 +0700

    [core] Add main entry point class

---
+++ core/src/main/java/com/vcc/bigdata/Main.java
@@ -0,0 +1,58 @@
+package com.vcc.bigdata;
+
+import com.vcc.bigdata.common.config.Configuration;
+import com.vcc.bigdata.common.utils.CLIArgumentParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * TODO: Class description here.
+ *
+ * @author <a href="https://github.com/tjeubaoit">tjeubaoit</a>
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        CLIArgumentParser parser = new CLIArgumentParser.Builder()
+                .addOpt(null, "logging.level", true, "Log level", false)
+                .addOpt(null, "logging.path", true, "Log path", false)
+                .addOpt(null, "app.conf", true, "App config path", false)
+                .addOpt(null, "class", true, "Main class", true)
+                .build();
+
+        try {
+            CLIArgumentParser.Result cmd = parser.parse(args);
+            initSystemProperties(cmd.getOptions());
+            initLogger();
+            invokeMainClass(cmd.getOptionValue("class"), cmd.getAppArgs());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            parser.printHelpUsage();
+            System.exit(1);
+        }
+    }
+
+    private static void initSystemProperties(Map<String, String> options) {
+        options.remove("class");
+        for (Map.Entry<String, String> e : options.entrySet()) {
+            if (e.getValue() == null) continue;
+            System.setProperty(e.getKey(), e.getValue());
+        }
+    }
+
+    private static void initLogger() {
+        Configuration config = new Configuration();
+        Configuration.setSystemPropertiesFromConfig(config, "logging.level", "logging.path");
+        Logger logger = LoggerFactory.getLogger(Configuration.class);
+        logger.info(config.toString());
+    }
+
+    private static void invokeMainClass(String className, String[] args) throws Exception {
+        Class<?> clazz = Main.class.getClassLoader().loadClass(className);
+        Method method = clazz.getMethod("main", String[].class);
+        method.invoke(null, (Object) args);
+    }
+}

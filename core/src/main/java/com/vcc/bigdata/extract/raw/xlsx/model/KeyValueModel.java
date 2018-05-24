feaed1a4631badab16b77915e86e748ddaba1f39
tjeubaoit <anhtn.bk@gmail.com>
2018-05-24 23:41:21 +0700

    [core] Add raw file extractors

---
+++ core/src/main/java/com/vcc/bigdata/extract/raw/xlsx/model/KeyValueModel.java
@@ -0,0 +1,34 @@
+package com.vcc.bigdata.extract.raw.xlsx.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+//@JsonIgnoreProperties({"key"})
+public class KeyValueModel {
+    private String key;
+    private List<String> value;
+
+    public KeyValueModel() {
+    }
+
+    public KeyValueModel(String key, List<String> value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public List<String> getValue() {
+        return value;
+    }
+
+    public void setValue(List<String> value) {
+        this.value = value;
+    }
+}

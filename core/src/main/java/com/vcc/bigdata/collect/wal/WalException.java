4ba85f98b421c42e518bfbda08102d793589e238
tjeubaoit <anhtn.bk@gmail.com>
2018-06-05 22:07:36 +0700

    [core] Prepare for WAL collector: Add WAL file handler

---
+++ core/src/main/java/com/vcc/bigdata/collect/wal/WalException.java
@@ -0,0 +1,24 @@
+package com.vcc.bigdata.collect.wal;
+
+/**
+ * TODO: Class description here.
+ *
+ * @author <a href="https://github.com/tjeubaoit">tjeubaoit</a>
+ */
+public class WalException extends RuntimeException {
+
+    public WalException() {
+    }
+
+    public WalException(String message) {
+        super(message);
+    }
+
+    public WalException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WalException(Throwable cause) {
+        super(cause);
+    }
+}

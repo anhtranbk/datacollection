008515a6d9ac7ffe93170a610b3afe0d8053b87f
tjeubaoit <anhtn.bk@gmail.com>
2018-06-20 20:18:19 +0700

    [core] Add Thrift facebook avatar fetcher

---
+++ core/src/main/java/com/vcc/bigdata/collect/fbavt/ThriftFetcher.java
@@ -0,0 +1,45 @@
+package com.vcc.bigdata.collect.fbavt;
+
+import com.vcc.bigdata.common.config.Properties;
+import com.vcc.bigdata.platform.thrift.ThriftClient;
+import com.vcc.bigdata.platform.thrift.ThriftClientProvider;
+import com.vcc.bigdata.platform.thrift.ThriftRuntimeException;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TMultiplexedProtocol;
+import thriftgen.FbAvatarService;
+
+/**
+ * TODO: Class description here.
+ *
+ * @author <a href="https://github.com/tjeubaoit">tjeubaoit</a>
+ */
+public class ThriftFetcher implements Fetcher {
+
+    private Properties props;
+    private ThreadLocal<FbAvatarService.Client> clientThreadLocal = new ThreadLocal<>();
+
+    @Override
+    public void configure(Properties p) {
+        this.props = p;
+    }
+
+    @Override
+    public String fetch(String id) {
+        try {
+            FbAvatarService.Client client = clientThreadLocal.get();
+            if (client == null) {
+                client = newClient(props);
+                clientThreadLocal.set(client);
+            }
+            return client.fetchAvatarUrl(id);
+        } catch (TException e) {
+            throw new ThriftRuntimeException(e);
+        }
+    }
+
+    private static FbAvatarService.Client newClient(Properties props) {
+        ThriftClient thriftClient = ThriftClientProvider.getOrCreate(Thread.currentThread().getName(), props);
+        return new FbAvatarService.Client(
+                new TMultiplexedProtocol(thriftClient.getProtocol(), "FbAvatarService"));
+    }
+}

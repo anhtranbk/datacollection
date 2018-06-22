a04d61c498a0e027cac48446eafa676d3495c599
tjeubaoit <anhtn.bk@gmail.com>
2018-06-22 22:22:21 +0700

    [core] Add feature Remote Id generator

---
+++ core/src/main/java/com/vcc/bigdata/collect/idgen/MockRemoteIdGenerator.java
@@ -0,0 +1,22 @@
+package com.vcc.bigdata.collect.idgen;
+
+import com.vcc.bigdata.common.config.Properties;
+
+import java.util.List;
+
+/**
+ * TODO: Class description here.
+ *
+ * @author <a href="https://github.com/tjeubaoit">tjeubaoit</a>
+ */
+public class MockRemoteIdGenerator implements RemoteIdGenerator {
+
+    @Override
+    public long generate(List<String> seeds, long defVal) {
+        return defVal;
+    }
+
+    @Override
+    public void configure(Properties p) {
+    }
+}
---
+++ core/src/main/java/com/vcc/bigdata/collect/idgen/SafeIdGenServer.java
@@ -0,0 +1,72 @@
+package com.vcc.bigdata.collect.idgen;
+
+import com.vcc.bigdata.common.cache.Cache;
+import com.vcc.bigdata.common.cache.LRUCache;
+import com.vcc.bigdata.common.config.Configuration;
+import com.vcc.bigdata.common.config.Properties;
+import com.vcc.bigdata.common.utils.Strings;
+import com.vcc.bigdata.platform.thrift.ThriftServer;
+import org.apache.thrift.TException;
+import org.apache.thrift.TMultiplexedProcessor;
+import org.apache.thrift.TProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import thriftgen.IdGenerator;
+
+import java.util.List;
+
+/**
+ * TODO: Class description here.
+ *
+ * @author <a href="https://github.com/tjeubaoit">tjeubaoit</a>
+ */
+public class SafeIdGenServer {
+
+    public static final int DEFAULT_MAX_CACHE_SIZE = 10 * 1000 * 1000;
+    private static final Logger logger = LoggerFactory.getLogger(SafeIdGenServer.class);
+
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+        ThriftServer server = new ThriftServer(conf, initThriftProcessor(conf));
+        server.start();
+    }
+
+    static TProcessor initThriftProcessor(Properties props) {
+        int maxCacheSize = props.getIntProperty("idgen.max.cache.size", DEFAULT_MAX_CACHE_SIZE);
+        TMultiplexedProcessor processor = new TMultiplexedProcessor();
+        processor.registerProcessor("IdGenerator", new IdGenerator.Processor<>(
+                new IdGeneratorHandler(maxCacheSize)));
+
+        return processor;
+    }
+
+    private static class IdGeneratorHandler implements IdGenerator.Iface {
+
+        private final Cache<String, Long> cache;
+
+        IdGeneratorHandler(int maxSize) {
+            this.cache = new LRUCache<>(maxSize);
+        }
+
+        @Override
+        public long generate(List<String> seeds, long def_val) throws TException {
+            synchronized (this) {
+                long finalVal = def_val;
+                for (String seed : seeds) {
+                    Long val = cache.get(seed);
+                    if (val != null) {
+                        finalVal = val;
+                        break;
+                    }
+                }
+
+                logger.info(Strings.format("Seeds: %s, val: %d", seeds, finalVal));
+                for (String seed : seeds) {
+                    cache.put(seed, finalVal);
+                }
+
+                return finalVal;
+            }
+        }
+    }
+}

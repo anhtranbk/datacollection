bf47d156e8c63abc69bb4f2eddaa66745830e953
tjeubaoit <anhtn.bk@gmail.com>
2018-07-25 21:16:56 +0700

    [core] Add org extractor

---
+++ core/src/main/java/com/vcc/bigdata/extract/mongo/OrgProfileExtractor.java
@@ -0,0 +1,74 @@
+package com.vcc.bigdata.extract.mongo;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
+import com.vcc.bigdata.common.config.Configuration;
+import com.vcc.bigdata.common.mb.MockMsgBrokerFactory;
+import com.vcc.bigdata.extract.DataStream;
+import com.vcc.bigdata.extract.Extractor;
+import com.vcc.bigdata.extract.model.GenericModel;
+import org.bson.Document;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class OrgProfileExtractor extends MongoExtractor {
+
+    private static final long MIN_EPOCH = 1262278800000L;
+    private static final DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public OrgProfileExtractor(Configuration config) {
+        super("org", config);
+    }
+
+    @Override
+    protected GenericModel extractData(Document document) {
+        String id = document.getString("_id");
+        String type = GenericModel.TYPE_ORG;
+
+        document.put("_id", id);
+        return new GenericModel(id, type, document);
+    }
+
+    @Override
+    protected DataStream<Document> openDataStream() {
+        String strIndex = loadIndex();
+        Date lastIndex;
+        try {
+            lastIndex = df.parse(strIndex);
+        } catch (Exception e) {
+            lastIndex = new Date(MIN_EPOCH);
+        }
+        logger.info("Last index: " + df.format(lastIndex));
+
+        return new MongoDataStream(new MongoFetcher() {
+            @Override
+            public MongoCursor<Document> fetchNextDocs(Object fromIndex) {
+                return database.getCollection(collection)
+                        .find(Filters.gt("GetDate", fromIndex))
+                        .sort(new BasicDBObject("GetDate", 1))
+                        .limit(batchSize)
+                        .iterator();
+            }
+
+            @Override
+            public Object fetchIndex(Document doc) {
+                return doc.getDate("GetDate");
+            }
+        }, lastIndex);
+    }
+
+    @Override
+    protected void onRecordProcessed(GenericModel model, long queueOrder, Object attachment) {
+        Document doc = (Document) attachment;
+        storeIndex(df.format(doc.getDate("GetDate")), queueOrder);
+    }
+
+    public static void main(String[] args) {
+        Extractor extractor = new OrgProfileExtractor(new Configuration());
+        extractor.setMsgBrokerFactory(new MockMsgBrokerFactory());
+        extractor.start();
+    }
+}

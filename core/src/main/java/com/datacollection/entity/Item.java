b26beaf595b2368f0f98b7c0a2b0f0c2c8ab959d
tjeubaoit <anhtn.bk@gmail.com>
2018-08-26 11:15:33 +0700

    [core] Add base entities

---
+++ core/src/main/java/com/datacollection/entity/Item.java
@@ -0,0 +1,60 @@
+package com.datacollection.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class Item {
+
+    private String id;
+    private String type;
+    private String scope;
+
+    public Item() {
+    }
+
+    public Item(String id, String type) {
+        this(id, type, "default");
+    }
+
+    public Item(String id, String type, String scope) {
+        this.id = id;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id='" + id + '\'' +
+                ", type='" + type + '\'' +
+                ", scope='" + scope + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Item setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Item setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+}
---
+++ core/src/main/java/com/datacollection/entity/MetaData.java
@@ -0,0 +1,70 @@
+package com.datacollection.entity;
+
+import java.util.List;
+
+public class MetaData extends Item {
+
+    private String name;
+    private String description;
+    private List<String> tags;
+    private List<String> systemTags;
+    private boolean enabled;
+    private boolean hidden;
+    private boolean readOnly;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public List<String> getSystemTags() {
+        return systemTags;
+    }
+
+    public void setSystemTags(List<String> systemTags) {
+        this.systemTags = systemTags;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isHidden() {
+        return hidden;
+    }
+
+    public void setHidden(boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    public boolean isReadOnly() {
+        return readOnly;
+    }
+
+    public void setReadOnly(boolean readOnly) {
+        this.readOnly = readOnly;
+    }
+}

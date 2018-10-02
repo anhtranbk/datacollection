a83537e8228e1ac1958755349557ee617d7d29b0
tjeubaoit <anhtn.bk@gmail.com>
2018-10-02 23:19:49 +0700

    [plugins/kafka] Add Kafka flush

--- plugins/kafka/src/main/java/com/datacollection/platform/kafka/KafkaBrokerWriter.java
+++ plugins/kafka/src/main/java/com/datacollection/platform/kafka/KafkaBrokerWriter.java
@@ -37,6 +37,11 @@ public class KafkaBrokerWriter implements BrokerWriter {
     }
 
     @Override
+    public void flush() {
+        this.producer.flush();
+    }
+
+    @Override
     public void close() {
         this.producer.close();
     }

1b834a0eb6f144f98cc892b30b8dcfa72b08de36
tjeubaoit <anhtn.bk@gmail.com>
2018-10-19 23:58:24 +0700

    [common] Add func common classes

---
+++ common/src/main/java/com/datacollection/common/func/Func0.java
@@ -0,0 +1,7 @@
+package com.datacollection.common.func;
+
+@FunctionalInterface
+public interface Func0<R> {
+
+    R apply();
+}
---
+++ common/src/main/java/com/datacollection/common/func/Func1.java
@@ -0,0 +1,6 @@
+package com.datacollection.common.func;
+
+import java.util.function.Function;
+
+public interface Func1<T, R> extends Function<T, R> {
+}
